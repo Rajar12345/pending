1b3bae2f97daab6f5367d8ba5ea922c5637d592c
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2023-10-31 09:20:25 +0530

    Update pend.js

--- pend.js
+++ pend.js
@@ -1,28 +1,49 @@
+// Step 1: Add event listener to "Approve" button
+const approveButtons = document.querySelectorAll('.approve-button');
+approveButtons.forEach(button => {
+  button.addEventListener('click', handleApproval);
+});
+
+function handleApproval(event) {
+  // Step 2: Retrieve row data
+  const row = event.target.closest('tr');
+  const leaveId = row.querySelector('td:nth-child(1)').textContent;
+  const visitPlace = row.querySelector('td:nth-child(2)').textContent;
+  const reason = row.querySelector('td:nth-child(3)').textContent;
+  const leaveType = row.querySelector('td:nth-child(4)').textContent;
+  const from = row.querySelector('td:nth-child(5)').textContent;
+  const to = row.querySelector('td:nth-child(6)').textContent;
+
+  // Step 3: Remove row from table
+  row.remove();
+
+  // Step 4: Generate serial number
+  const serialNumber = generateSerialNumber();
+
+  // Step 5: Store serial number and request details
+  storeApprovalDetails(serialNumber, leaveId, visitPlace, reason, leaveType, from, to);
+
+  // Display serial number to the student
+  alert(Your request has been approved. Serial number: ${serialNumber});
+}
 
 function generateSerialNumber() {
-    // Generate a unique serial number, e.g., based on timestamp
-    return Date.now().toString();
-}
-// Function to show the modal with a specific message
-function showModal(message) {
-    const modal = document.getElementById('myModal');
-    const popupMessage = document.getElementById('popupMessage');
-    popupMessage.textContent = message;
-    modal.style.display = 'block';
+  // Generate a unique identifier (e.g., timestamp or random number)
+  // You can use a library or custom logic to generate the serial number
+  // For simplicity, let's use the current timestamp
+  const timestamp = Date.now();
+  return SN-${timestamp};
 }
 
-// Close the modal when clicking outside of it
-window.onclick = function(event) {
-    const modal = document.getElementById('myModal');
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
+function storeApprovalDetails(serialNumber, leaveId, visitPlace, reason, leaveType, from, to) {
+  // Store the approval details in a database or any other persistent storage
+  // You can use AJAX or any backend technology to handle the storage process
+  // Here, we'll just log the details to the console
+  console.log(Serial Number: ${serialNumber});
+  console.log(Leave ID: ${leaveId});
+  console.log(Visit Place: ${visitPlace});
+  console.log(Reason: ${reason});
+  console.log(Leave Type: ${leaveType});
+  console.log(From: ${from});
+  console.log(To: ${to});
 }
-
-// Handle approval action
-// After approval, call showModal with an appropriate message
-showModal('Request Approved');
-
-// Handle disapproval action
-// After disapproval, call showModal with an appropriate message
-showModal('Request Disapproved');
